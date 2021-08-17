d6413e4618e61252c5593313a6b6df733b434770
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-08-17 15:33:20 -0300

    feat: first test of route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const app = express()
 
 app.use(express.json())
+
+app.get('/test', (req: Request, res: Response) => {
+  res.json({ success: true })
+})
 
 app.listen(7000, () => console.log("Server is running!"))
