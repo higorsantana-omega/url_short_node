bd426d457c405f3b70ca20670e6237edec81caad
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-08-17 16:33:36 -0300

    feat: registry url in database

--- src/controllers/URLController.ts
+++ src/controllers/URLController.ts
@@ -1,3 +1,4 @@
+import { URLModel } from "../database/model/URL";
 import { Request, Response } from "express";
 import { nanoid } from "nanoid";
 
@@ -6,10 +7,18 @@ import { config } from '../config/Constants'
 class URLController {
   async shorten(req: Request, res: Response): Promise<void>{
     const { originUrl } = req.body
+    const url = await URLModel.findOne({ originUrl })
+
+    if (url) {
+      res.json(url)
+      return
+    }
+
     const hash = nanoid()
     const shortURL = `${config.API_URL}/${hash}`
+    const newURL = await URLModel.create({ hash, shortURL, originUrl })
     
-    res.json({ originUrl, hash, shortURL })
+    res.json(newURL)
   }
 
   async redirect(req: Request, res: Response): Promise<void>{
