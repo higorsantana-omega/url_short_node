03853d24e1e385aa71bc514d120659e8ca49f02b
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-08-17 16:02:48 -0300

    feat: create redirect

--- src/controllers/URLController.ts
+++ src/controllers/URLController.ts
@@ -11,6 +11,16 @@ class URLController {
     
     res.json({ originUrl, hash, shortURL })
   }
+
+  async redirect(req: Request, res: Response): Promise<void>{
+    const { hash } = req.params
+    const url = {
+      originURL: "https://cloud.mongodb.com/v2/611bf6731f7cdb5c4a457dd7#clusters/connect?clusterId=Cluster0",
+      hash: "VB1J7ZQaBGqp4OBMnTKnf",
+      shortURL: "http://localhost:7000/VB1J7ZQaBGqp4OBMnTKnf"
+    }
+    res.redirect(url.originURL)
+  }
 }
 
 export { URLController }
