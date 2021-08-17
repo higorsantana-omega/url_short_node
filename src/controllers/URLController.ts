edf762f8aab95b7b89e1f69a46be33d51a1af06e
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-08-17 16:44:02 -0300

    feat: redirect

--- src/controllers/URLController.ts
+++ src/controllers/URLController.ts
@@ -2,34 +2,36 @@ import { URLModel } from "../database/model/URL";
 import { Request, Response } from "express";
 import { nanoid } from "nanoid";
 
-import { config } from '../config/Constants'
+import { config } from "../config/Constants";
 
 class URLController {
-  async shorten(req: Request, res: Response): Promise<void>{
-    const { originUrl } = req.body
-    const url = await URLModel.findOne({ originUrl })
+  async shorten(req: Request, res: Response): Promise<void> {
+    const { originUrl } = req.body;
+    const url = await URLModel.findOne({ originUrl });
 
     if (url) {
-      res.json(url)
-      return
+      res.json(url);
+      return;
     }
 
-    const hash = nanoid()
-    const shortURL = `${config.API_URL}/${hash}`
-    const newURL = await URLModel.create({ hash, shortURL, originUrl })
-    
-    res.json(newURL)
+    const hash = nanoid();
+    const shortURL = `${config.API_URL}/${hash}`;
+    const newURL = await URLModel.create({ hash, shortURL, originUrl });
+
+    res.json(newURL);
   }
 
-  async redirect(req: Request, res: Response): Promise<void>{
-    const { hash } = req.params
-    const url = {
-      originURL: "https://cloud.mongodb.com/v2/611bf6731f7cdb5c4a457dd7#clusters/connect?clusterId=Cluster0",
-      hash: "VB1J7ZQaBGqp4OBMnTKnf",
-      shortURL: "http://localhost:7000/VB1J7ZQaBGqp4OBMnTKnf"
+  async redirect(req: Request, res: Response): Promise<void> {
+    const { hash } = req.params;
+    const url = await URLModel.findOne({ hash });
+
+    if (url) {
+      res.redirect(url.originUrl);
+      return;
     }
-    res.redirect(url.originURL)
+
+    res.status(400).json({ error: "URL not found" });
   }
 }
 
-export { URLController }
+export { URLController };
